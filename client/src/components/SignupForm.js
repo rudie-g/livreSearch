8742fe4d888490fbe35acd56d548269c18cf79bb
Rudie G <rauoldm@gmail.com>
2021-09-10 10:27:32 -0500

    troubleshooting saved books not appearing

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import {useMutation} from "@apollo/client"
+import { ADD_USER } from '../utils/mutations';
 
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,6 +12,8 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [createUser, {error}] = useMutation(ADD_USER)
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,13 +31,15 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const {data} = await createUser({
+        variables: userFormData
+      })
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      if(error) {
+        console.log(error)
       }
 
-      const { token, user } = await response.json();
+      const { token, user } = data.addUser
       console.log(user);
       Auth.login(token);
     } catch (err) {
