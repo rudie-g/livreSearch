49f138984a3e14a0422dbcfba51979f07ded7772
Rudie G <rauoldm@gmail.com>
2021-09-13 14:07:16 -0500

    somehow graphql wasnt installed

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 let server = null
 
